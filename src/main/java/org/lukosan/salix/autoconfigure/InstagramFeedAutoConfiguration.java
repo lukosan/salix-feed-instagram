3f3b1f1f311a35fa2deb4fd75219fbfe785bd871
Luke Pryor <lukosan@gmail.com>
2015-11-06 09:10:14 +0000

    Added MVC project and publishers

--- src/main/java/org/lukosan/salix/autoconfigure/InstagramFeedAutoConfiguration.java
+++ src/main/java/org/lukosan/salix/autoconfigure/InstagramFeedAutoConfiguration.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import javax.annotation.PostConstruct;
-import javax.servlet.Servlet;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -14,9 +13,7 @@ import org.lukosan.salix.feed.instagram.InstagramProperties;
 import org.lukosan.salix.feed.instagram.SalixFeedInstagram;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -29,12 +26,16 @@ import org.springframework.context.annotation.Configuration;
 public class InstagramFeedAutoConfiguration {
 
 	@Configuration
-	@ConditionalOnClass({ Servlet.class })
-	@ConditionalOnWebApplication
 	@ComponentScan(basePackages="org.lukosan.salix.feed.instagram")
 	public static class InstagramFeedConfiguration {
 		
 		private static final Log logger = LogFactory.getLog(InstagramFeedConfiguration.class);
+		
+		@PostConstruct
+		public void postConstruct() {
+			if(logger.isInfoEnabled())
+				logger.info("PostConstruct " + getClass().getSimpleName());
+		}
 		
 		@Autowired
 		private InstagramProperties properties;
@@ -59,11 +60,6 @@ public class InstagramFeedAutoConfiguration {
 					.map(config -> new SalixFeedInstagram(config)).collect(Collectors.toList());
 		}
 		
-		@PostConstruct
-		public void postConstruct() {
-			if(logger.isInfoEnabled())
-				logger.info("PostConstruct " + getClass().getSimpleName());
-		}
 	}
 	
 }
